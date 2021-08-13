eb6040bce79558bb9df0f4a93e006a0dec11c53c
Hasan Yousef <46838626+hajsf@users.noreply.github.com>
2021-08-13 18:40:06 +0300

    Add files via upload

---
+++ go/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "C"
+
+// other imports should be seperate from the special Cgo import
+import (
+	"embed"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+// Embed the file content as string.
+//go:embed title.txt
+var title string
+
+// Embed the entire directory.
+//go:embed templates
+var indexHTML embed.FS
+
+//go:embed static
+var staticFiles embed.FS
+
+//export server
+func server() {
+	c := make(chan bool)
+
+	// http.FS can be used to create a http Filesystem
+	var staticFS = http.FS(staticFiles)
+	fs := http.FileServer(staticFS)
+
+	// Serve static files
+	http.Handle("/static/", fs)
+
+	go func() {
+		log.Println(http.ListenAndServe("127.0.0.1:6060", nil))
+		<-c
+	}()
+
+	http.HandleFunc("/", handler)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var path = r.URL.Path
+	// Note the call to ParseFS instead of Parse
+	t, err := template.ParseFS(indexHTML, "templates/index.html.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Add("Content-Type", "text/html")
+
+	// respond with the output of template execution
+	t.Execute(w, struct {
+		Title    string
+		Response string
+	}{Title: title, Response: path})
+}
+
+func main() {}
